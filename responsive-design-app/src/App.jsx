ce035d987c47f95f117be6c017f03f80470eca17
Kumar Shreyash <shreyash.sinha35@gmail.com>
2025-07-10 02:13:06 +0530

    asses

---
+++ responsive-design-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './index.css'
+import Respo from './components/Respo'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Respo/>
+       </div>
+    </>
+  )
+}
+
+export default App
---
+++ responsive-design-app/src/components/Respo.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+function Respo(){
+return (
+    <>
+    <div>
+        <h1 className="text-4xl font-bold text-center p-4">Our Clients Speak</h1>
+        <h3 className="text-xl font-bold text-center text-gray-600">We have been working with clients around the world</h3>
+    </div>
+    <div className="flex justify-evenly m-10 gap-10">
+        <div className="w-100 text-center  shadow-[0px_5px_15px_rgba(0,0,0,0.35)] p-10">
+            <img className="h-32 w-32 rounded-full mx-auto mb-4" src="https://img.freepik.com/free-photo/brunette-business-woman-with-wavy-long-hair-blue-eyes-stands-holding-notebook-hands_197531-343.jpg?semt=ais_hybrid&w=740" alt="" />
+            <p className="text-xl font-bold mb-1">Jane Cooper</p>
+            <p className="font-bold text-gray-600 mb-2">CEO at ABC Corporation</p>
+            <div >
+                <h2 className="text-2xl font-bold mb-3">Efficient Collaborating</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores magnam magni enim veniam similique assumenda deleniti facere consectetur, suscipit iure consequatur vitae quam eum consequuntur natus autem beatae harum repudiandae.</p>
+            </div>
+        </div >
+        <div className="w-100 text-center shadow-[0px_5px_15px_rgba(0,0,0,0.35)] p-10">
+            <img className="h-32 w-32 rounded-full mx-auto mb-4" src="https://img.freepik.com/free-photo/brunette-business-woman-with-wavy-long-hair-blue-eyes-stands-holding-notebook-hands_197531-343.jpg?semt=ais_hybrid&w=740" alt="" />
+            <p className="text-xl font-bold mb-1">Jane Cooper</p>
+            <p className="font-bold text-gray-600 mb-2">CEO at ABC Corporation</p>
+            <h2 className="text-2xl font-bold mb-3">Intuitive Design</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores magnam magni enim veniam similique assumenda deleniti facere consectetur, suscipit iure consequatur vitae quam eum consequuntur natus autem beatae harum repudiandae.</p>
+           
+        </div>
+        <div className="w-100 text-center shadow-[0px_5px_15px_rgba(0,0,0,0.35)] p-10">
+            <img className="h-32 w-32 rounded-full mx-auto mb-4" src="https://img.freepik.com/free-photo/brunette-business-woman-with-wavy-long-hair-blue-eyes-stands-holding-notebook-hands_197531-343.jpg?semt=ais_hybrid&w=740" alt="" />
+            <p className="text-xl font-bold mb-1">Jane Cooper</p>
+            <p className="font-bold text-gray-600 mb-2">CEO at ABC Corporation</p>
+            <h2 className="text-2xl font-bold mb-3">Mindblowing Service</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores magnam magni enim veniam similique assumenda deleniti facere consectetur, suscipit iure consequatur vitae quam eum consequuntur natus autem beatae harum repudiandae.</p>
+           
+        </div>
+    </div>
+    </>
+)
+}
+export default Respo
